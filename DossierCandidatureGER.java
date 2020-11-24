aadb6acf244ddad804de87295beece5e1fde4456
Abdou Afifi <afifiabdou@gmail.com>
2020-11-24 17:42:03 +0100

    Add files via upload

---
+++ DossierCandidatureGER.java
@@ -0,0 +1,45 @@
+public class DossierCandidatureGER{
+  // Ici des attributs
+  private String nom;
+  private String numero;
+  
+	public String getNom()
+	{
+		 return nom; 
+	}
+	
+	public String getNumero()
+	{
+		 return numero; 
+	}
+	
+	private  setNom(String n)
+	{
+		  nom=n; 
+	}
+	private  setNumero(String n)
+	{
+		  numero=n; 
+	}
+	
+
+  // constructeur
+  public DossierCandidatureGER(String nom, String numero)
+  {
+  }
+  public void enregistrerEmargementEpreuveEcrite()
+  {
+  }
+  public void enregistrerNoteEpreuveEcrite(int note){
+  }
+  public void publierResultatEcrit(){
+  }
+  public void enregistrerNoteEpreuveOrale(int note){
+  }
+  public void enregistrerAbsenceALOral(){
+  }
+  public void publierResultatFinal(){
+  }
+  public String toString(){
+  }
+}
